b59106adbadb51de071662694a7183dab5ffd347
Matthew Parker <cadburylion@outlook.com>
2017-04-21 16:23:24 -0700

    built out html scaffold and added a form with several fields and buttons and built two calculator functions in javascript and added a reset in css

---
+++ calc.js
@@ -0,0 +1,9 @@
+'use strict';
+
+function cnvrtb() {
+  document.getElementById('theIBox').value=document.getElementById("theBox").value/2.54;
+}
+
+function cnvrta() {
+  document.getElementById('theCBox').value=document.getElementById("theBox").value*2.54;
+}
